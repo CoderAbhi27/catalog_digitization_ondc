dd8080c0f64d5bf9d88d55f14f8e3e24c40e13e9
CoderAbhi27 <abhinavkr327@gmail.com>
2024-02-07 19:13:56 +0530

    CoderAbhi27

--- lib/pages/get_started.dart
+++ lib/pages/get_started.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GetStarted extends StatelessWidget {
   const GetStarted({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double baseWidth = 390;
+    double screen_height = MediaQuery.of(context).size.height;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Loda'),
-        backgroundColor: Colors.amber,
+      backgroundColor: Colors.grey[800],
+      // backgroundColor: Color(0xff272727),
+      body: Container(
+        // getchewingMBj (2:368)
+        // padding: EdgeInsets.fromLTRB(34.5*fem, 14.33*fem, 34.5*fem, 8*fem),
+
+        margin: EdgeInsets.fromLTRB(87.5*fem, screen_height*0.85, 87.5*fem, 0),
+        width: double.infinity,
+        height: 43*fem,
+        // decoration: BoxDecoration (
+        //   color: Color(0xff7e2ece),
+        //   borderRadius: BorderRadius.circular(8*fem),
+        // ),
+        child: ElevatedButton(
+          style: ButtonStyle(
+            side: MaterialStateProperty.all(BorderSide(
+              color: Colors.black
+            )),
+            padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: 8*fem)),
+            backgroundColor: MaterialStateProperty.all(Color(0xff7e2ece)),
+          ),
+          onPressed: (){
+            Navigator.pushReplacementNamed(context, '/sign_in');
+          },
+          child: Center(
+            child: Text(
+              'Get Started !',
+              textAlign: TextAlign.center,
+              style:  GoogleFonts.aBeeZee(
+                // 'Raleway',
+                fontSize: 16*ffem,
+                fontWeight: FontWeight.w700,
+                height: 1.175*ffem/fem,
+                color: Color(0xffffffff),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -8,8 +9,92 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double baseWidth = 390;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
+    return Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'TutorialKart',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30),
+                  )),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(fontSize: 20),
+                  )),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'User Name',
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: TextField(
+                  obscureText: true,
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  //forgot password screen
+                },
+                child: const Text('Forgot Password',),
+              ),
+              Container(
+                  height: 50,
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: ElevatedButton(
+                    child: const Text('Login'),
+                    onPressed: () {
+                      print(nameController.text);
+                      print(passwordController.text);
+                    },
+                  )
+              ),
+              Row(
+                children: <Widget>[
+                  const Text('Does not have account?'),
+                  TextButton(
+                    child: const Text(
+                      'Sign in',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      //signup screen
+                    },
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+            ],
+          )),
+    );
   }
 }
