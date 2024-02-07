5df9ce1b68f5bf6d1aa87d120911534680661493
aryan <aryanrdat@gmail.com>
2024-02-07 16:58:17 +0530

    com

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,6 +10,6 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
