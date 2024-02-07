70c3b22f58bec59fa9692ef54c86e8c51b77d52c
CoderAbhi27 <abhinavkr327@gmail.com>
2024-02-08 03:36:13 +0530

    CoderAbhi27

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -1,5 +1,7 @@
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -12,89 +14,179 @@ class _SignInState extends State<SignIn> {
 
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  bool hidePassword = true;
 
   @override
   Widget build(BuildContext context) {
     double baseWidth = 390;
     double fem = MediaQuery.of(context).size.width / baseWidth;
-    double ffem = fem * 0.97;
+    // double ffem = fem * 0.97;
+
     return Scaffold(
+      appBar: AppBar(
+        title: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'ONDC',
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 30),
+            )),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+      ),
+      backgroundColor: Colors.grey[800],
       body: Padding(
           padding: const EdgeInsets.all(10),
           child: ListView(
             children: <Widget>[
+
               Container(
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.all(10),
-                  child: const Text(
-                    'TutorialKart',
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 30),
-                  )),
-              Container(
+                margin: EdgeInsets.only(top: fem*70, bottom: fem*120),
                   alignment: Alignment.center,
                   padding: const EdgeInsets.all(10),
                   child: const Text(
                     'Sign in',
-                    style: TextStyle(fontSize: 20),
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                    ),
                   )),
               Container(
                 padding: const EdgeInsets.all(10),
                 child: TextField(
+                  style: TextStyle(
+
+                    color: Colors.white,
+                  ),
                   controller: nameController,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'User Name',
+                    icon: Icon(Icons.person),
+                    iconColor: Colors.white,
+                    labelStyle: TextStyle(color: Colors.white),
                   ),
                 ),
               ),
               Container(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0.0),
                 child: TextField(
-                  obscureText: true,
+                  obscureText: hidePassword,
                   controller: passwordController,
-                  decoration: const InputDecoration(
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Password',
+                    icon: Icon(Icons.password),
+                    iconColor: Colors.white,
+                    labelStyle: TextStyle(color: Colors.white),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        hidePassword ? Icons.visibility_off : Icons.visibility
+                      ),
+                      color: Colors.white,
+                      onPressed: (){
+                        setState(() {
+                          print(hidePassword);
+                          if(hidePassword) hidePassword = false;
+                          else hidePassword = true;
+                          // hidePassword = !hidePassword;
+                        });
+                      },
+                    )
                   ),
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  //forgot password screen
-                },
-                child: const Text('Forgot Password',),
+              Container(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    //forgot password screen
+                  },
+                  child: Text('Forgot Password?  ',
+                    style: TextStyle(color: Colors.grey[200]),),
+                ),
               ),
               Container(
-                  height: 50,
-                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  height: 80,
+                  padding: const EdgeInsets.fromLTRB(10, 30.0, 10, 0),
                   child: ElevatedButton(
-                    child: const Text('Login'),
+                    child: const Text('SIGN IN', style: TextStyle(fontSize: 20.0)),
                     onPressed: () {
-                      print(nameController.text);
-                      print(passwordController.text);
+                      String name = nameController.text;
+                      String pass = passwordController.text;
+                      signIn(name, pass);
                     },
                   )
               ),
               Row(
                 children: <Widget>[
-                  const Text('Does not have account?'),
-                  TextButton(
-                    child: const Text(
-                      'Sign in',
-                      style: TextStyle(fontSize: 20),
+                  Container(
+                    // height: 40.0,
+                    padding: EdgeInsets.only(top: 10.5),
+                    child: const Text('Does not have an account?',
+                      style: TextStyle(color: Colors.white),),
+                  ),
+                  Container(
+                    height:40.0,
+                    // alignment: Alignment.topCenter,
+                    child: TextButton(
+
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.amber,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/sign_up');
+                      },
                     ),
-                    onPressed: () {
-                      //signup screen
-                    },
                   )
                 ],
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
               ),
             ],
           )),
     );
   }
+
+  Future<void> signIn(String email, String pass) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: pass
+      );
+      displaySnackBar('Signed in successfully!');
+      Navigator.pushReplacementNamed(context, '/home');
+
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        displaySnackBar('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        displaySnackBar('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  void displaySnackBar(String s) {
+    var snackdemo = SnackBar(
+      content: Text(s),
+      backgroundColor: Colors.green,
+      elevation: 10,
+      behavior: SnackBarBehavior.floating,
+      margin: EdgeInsets.all(5),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackdemo);
+  }
+
+
+
 }
--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -8,8 +9,203 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  bool hidePassword = true;
+  bool hideConfirmPassword = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double baseWidth = 390;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    // double ffem = fem * 0.97;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'ONDC',
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 30),
+            )),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+      ),
+      backgroundColor: Colors.grey[800],
+      body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+
+              Container(
+                  margin: EdgeInsets.only(top: fem*70, bottom: fem*120),
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Sign up',
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                    ),
+                  )),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  style: TextStyle(
+
+                    color: Colors.white,
+                  ),
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    icon: Icon(Icons.email),
+                    iconColor: Colors.white,
+                    labelStyle: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0.0),
+                child: TextField(
+                  obscureText: hidePassword,
+                  controller: passwordController,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      icon: Icon(Icons.password),
+                      iconColor: Colors.white,
+                      labelStyle: TextStyle(color: Colors.white),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                            hidePassword ? Icons.visibility_off : Icons.visibility
+                        ),
+                        color: Colors.white,
+                        onPressed: (){
+                          setState(() {
+                            hidePassword = !hidePassword;
+                          });
+                        },
+                      )
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0.0),
+                child: TextField(
+                  obscureText: hideConfirmPassword,
+                  controller: confirmPasswordController,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Confirm Password',
+                      icon: Icon(Icons.password),
+                      iconColor: Colors.white,
+                      labelStyle: TextStyle(color: Colors.white),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                            hideConfirmPassword ? Icons.visibility_off : Icons.visibility
+                        ),
+                        color: Colors.white,
+                        onPressed: (){
+                          setState(() {
+                            hideConfirmPassword = !hideConfirmPassword;
+                          });
+                        },
+                      )
+                  ),
+                ),
+              ),
+              Container(
+                  height: 80,
+                  padding: const EdgeInsets.fromLTRB(10, 30.0, 10, 0),
+                  child: ElevatedButton(
+                    child: const Text('SIGN UP', style: TextStyle(fontSize: 20.0)),
+                    onPressed: () {
+                      String email = nameController.text;
+                      String pass = passwordController.text;
+                      String confirmPass = confirmPasswordController.text;
+                      signUp(email, pass, confirmPass);
+                    },
+                  )
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    // height: 40.0,
+                    padding: EdgeInsets.only(top: 10.5),
+                    child: const Text('Already have an account?',
+                      style: TextStyle(color: Colors.white),),
+                  ),
+                  Container(
+                    height:40.0,
+                    // alignment: Alignment.topCenter,
+                    child: TextButton(
+
+                      child: const Text(
+                        'Sign in',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.amber,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/sign_in');
+                      },
+                    ),
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+            ],
+          )),
+    );
+  }
+
+  Future<void> signUp(String email, String pass, String confirmPass) async {
+    if(pass!=confirmPass){
+      displaySnackBar('Passwords does not match!');
+      return;
+    }
+    try {
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: pass,
+      );
+      displaySnackBar('Sign up successful');
+
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        displaySnackBar('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        displaySnackBar('The account already exists for that email.');
+      }
+    } catch (e) {
+      displaySnackBar(e.toString());
+    }
+
+  }
+
+  void displaySnackBar(String s) {
+    var snackdemo = SnackBar(
+      content: Text(s),
+      backgroundColor: Colors.green,
+      elevation: 10,
+      behavior: SnackBarBehavior.floating,
+      margin: EdgeInsets.all(5),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackdemo);
   }
 }
